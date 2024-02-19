d6008dcc40aee67d1e6252bb492c1097548dc0f3
immarchz <marcharissawat@gmail.com>
2024-02-19 19:13:39 +0700

    Update loadmodel.py

--- loadmodel.py
+++ loadmodel.py
@@ -11,6 +11,7 @@ import tensorflow as tf
 from keras.optimizers import Adam
 from keras.callbacks import ModelCheckpoint, EarlyStopping
 from keras.models import load_model
+from keras import backend as K
 
 def plot_(x,y1,y2,row,col,ind,title,xlabel,ylabel,label,isimage=False,color='r'):
 
@@ -78,6 +79,48 @@ def image2array(file_array,floder):
  image_array /= 255
  return np.array(image_array)
 
+def feature_extraction(model, data, layer = 4):
+
+    """
+    Creating a function to run the initial layers of the encoder model. (to get feature extraction from any layer of the model)
+    Arguments:
+    model - (Auto encoder model) - Trained model
+    data - (np.ndarray) - list of images to get feature extraction from trained model
+    layer - (int) - from which layer to take the features(by default = 4)
+    Returns:
+    pooled_array - (np.ndarray) - array of extracted features of given images
+    """
+
+    encoded = K.function([model.layers[0].input],[model.layers[layer].output])
+    encoded_array = encoded([data])[0]
+    pooled_array = encoded_array.max(axis=-1)
+    return encoded_array
+
+def get_batches(data, batch_size=1000):
+
+    """
+    Taking batch of images for extraction of images.
+    Arguments:
+    data - (np.ndarray or list) - list of image array to get extracted features.
+    batch_size - (int) - Number of images per each batch
+    Returns:
+    list - extracted features of each images
+    """
+
+    if len(data) < batch_size:
+        return [data]
+    n_batches = len(data) // batch_size
+    
+    # If batches fit exactly into the size of df.
+    if len(data) % batch_size == 0:
+        return [data[i*batch_size:(i+1)*batch_size] for i in range(n_batches)]   
+
+    # If there is a remainder.
+    else:
+        return [data[i*batch_size:min((i+1)*batch_size, len(data))] for i in range(n_batches+1)]
+
+
+
 train_data = image2array(train_files,"train\output")
 print("Length of training dataset:",train_data.shape)
 test_data = image2array(test_files,"test")
@@ -107,3 +150,29 @@ image = model.predict(sample_image)
 plot_(sample_image[0,:,:,:],'','',1,2,1,"Orginal Image","","","",True)
 plot_(image[0,:,:],'','',1,2,2,"Decoded Image","","","",True)
 plt.show()
+
+
+encoded = feature_extraction(model,train_data[:10],12)
+for index in [2,7,9]:   # 3 random images
+    plt.figure(figsize=(15,3))
+    plot_(train_data[index],'','',1,4,1,"Original Image","","",'',True)
+    plot_(encoded[index].mean(axis=-1),'','',1,4,2,"Encoded Mean","","",'',True)
+    plot_(encoded[index].max(axis=-1),'','',1,4,3,"Encoded Std","","",'',True)
+    plot_(encoded[index].std(axis=-1),'','',1,4,4,"Encoded Std","","",'',True)
+    plt.show()
+
+d = np.concatenate([train_data,test_data],axis=0)
+
+X_encoded = []
+i=0
+# Iterate through the full training set.
+for batch in get_batches(d, batch_size=300):
+    i+=1
+    # This line runs our pooling function on the model for each batch.
+    X_encoded.append(feature_extraction(model, batch),12)
+    
+X_encoded = np.concatenate(X_encoded)
+
+X_encoded_reshape = X_encoded.reshape(X_encoded.shape[0], X_encoded.shape[1]*X_encoded.shape[2]*X_encoded.shape[3])
+print('Encoded shape:', X_encoded_reshape.shape)
+np.save('/content/drive/My Drive/X_encoded_compressed.npy',X_encoded_reshape)
